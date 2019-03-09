77efd1d0ae1e2aaeb0d6ec147f5374b4b07b1086
FUZAIL KHAN <48386022+fuzailkhan2498@users.noreply.github.com>
2019-03-09 23:44:33 +0530

    Add files via upload

---
+++ basic/boolean.js
@@ -0,0 +1,15 @@
+let marks=1
+if(marks==10){
+    console.log(" VERY GOOD ")
+}
+else if(marks==5)
+{
+    console.log("WORK HARD")
+}
+else if(marks==3){
+    console.log("AVERAGE MARKS")
+}
+else{
+    console.log("poor performance")
+
+}
---
+++ basic/if.js
@@ -0,0 +1,11 @@
+let king="john"
+if(true){
+    let king ="joe"
+    if(true){
+        let king="rekesh"
+        console.log(king);
+        }
+}
+if(true){
+    console.log(king);
+}
